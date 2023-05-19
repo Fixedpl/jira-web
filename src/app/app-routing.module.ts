51b9c83b9e1241f19161b6dc51f344f2c9ddbbad
Marcin <303821@office.umcs.pl>
2023-05-19 12:55:06 +0200

    aaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: '', component: MainComponent, canActivate: [AuthGuard] },
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: '', component: MainComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileService } from './components/profile/profile.service';
+import { AvatarComponent } from './components/profile/avatar/avatar.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    MainComponent,
+    ProfileComponent,
+    AvatarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService , AuthGuard, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
